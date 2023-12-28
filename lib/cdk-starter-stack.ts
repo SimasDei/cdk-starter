3d20b82ebaca6291d87269cc153d082f4c18c549
Simas <simas@simas-dei.com>
2023-12-28 14:32:48 +0200

    feat: add cfn parameters

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -3,13 +3,13 @@ import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 class L3Bucket extends Construct {
-  constructor(scope: Construct, id: string, expiration?: cdk.Duration) {
+  constructor(scope: Construct, id: string, expiration?: number) {
     super(scope, id);
 
     new Bucket(this, id, {
       lifecycleRules: [
         {
-          expiration: expiration ?? cdk.Duration.days(2),
+          expiration: cdk.Duration.days(expiration ?? 2),
         },
       ],
     });
@@ -31,14 +31,25 @@ export class CdkStarterStack extends cdk.Stack {
       },
     });
 
-    new Bucket(this, 'MyL2Bucket', {
+    const durationParameter = new cdk.CfnParameter(this, 'MyL2BucketExpiration', {
+      type: 'Number',
+      default: 2,
+      minValue: 1,
+      maxValue: 10,
+    });
+
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [
         {
-          expiration: cdk.Duration.days(2),
+          expiration: cdk.Duration.days(durationParameter.valueAsNumber),
         },
       ],
     });
 
-    new L3Bucket(this, 'MyL3Bucket', cdk.Duration.days(2));
+    new L3Bucket(this, 'MyL3Bucket', durationParameter.valueAsNumber);
+
+    new cdk.CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName,
+    });
   }
 }
