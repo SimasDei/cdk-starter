808b2ace1dd7a07537c2760ded2a4a7210b9ae39
Simas <simas@simas-dei.com>
2023-12-28 13:47:55 +0200

    feat: add s3 bucket constructs

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration?: cdk.Duration) {
+    super(scope, id);
+
+    new Bucket(this, id, {
+      lifecycleRules: [
+        {
+          expiration: expiration ?? cdk.Duration.days(2),
+        },
+      ],
+    });
+  }
+}
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [
+          {
+            expirationInDays: 2,
+            status: 'Enabled',
+          },
+        ],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(2),
+        },
+      ],
+    });
+
+    new L3Bucket(this, 'MyL3Bucket', cdk.Duration.days(2));
   }
 }
