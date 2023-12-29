83e0e4d5b4ae72e7fa840e3acccc1e858d85fe8c
Simas <simas@simas-dei.com>
2023-12-29 14:39:08 +0200

    feat: add bucket tagger aspect

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,9 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PhotosStack } from '../lib/photos-stack';
 import { PhotosHandlerStack } from '../lib/photos-handler-stack';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
   targetBucketArn: photosStack.photosBucketArn,
 });
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
